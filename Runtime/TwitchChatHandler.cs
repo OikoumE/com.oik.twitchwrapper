7d0a17c258cf19cf741dc749cd84a347970a12d1
Leif Hagen <83828323+OikoumE@users.noreply.github.com>
2025-06-17 22:53:38 +0200

    added custom chat commands

--- Runtime/TwitchChatHandler.cs
+++ Runtime/TwitchChatHandler.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 public class TwitchChatHandler
 {
+    private const string FileName = "commands.json";
     private readonly EventSubWebsocket _client;
     private readonly Dictionary<CommandString, Action<ChatCommand>> _commands;
 
     private readonly string[] _ignoreNames;
+
+    private Dictionary<string, string> _customCommands;
     private Dictionary<CommandString, Action<ChatCommand>> _defaultCommands;
 
     public TwitchChatHandler(EventSubWebsocket client,
@@ -21,6 +25,7 @@ public class TwitchChatHandler
         _client = client;
         _commands = commands;
         SetupCommands();
+        AppendCustomCommands();
     }
 
     private void SetupCommands()
@@ -35,8 +40,81 @@ public class TwitchChatHandler
                 new CommandString("about"),
                 _ => TwitchApi.SendChatMessage("I am a Twitch bot running on Oik.TwitchWrapper!")
             },
-            { new CommandString(new[] { "command", "commands", "cmd", "cmds" }), AvailableCommands }
+            { new CommandString(new[] { "command", "commands", "cmd", "cmds" }), AvailableCommands },
+            { new CommandString(new[] { "cmdadd", "cmdAdd", "addcmd", "addCmd" }), AddCommand },
+            { new CommandString(new[] { "cmdedit", "cmdEdit", "editcmd", "editCmd" }), EditCommand }
         };
+    }
+
+    private void AppendCustomCommands()
+    {
+        _customCommands = LoadFromJson();
+        foreach (var (key, value) in _customCommands)
+            _defaultCommands.Add(new CommandString(key), _ => TwitchApi.SendChatMessage(value));
+    }
+
+    private static string GetPath()
+    {
+        return Path.Combine(Application.persistentDataPath, FileName);
+    }
+
+    public void SaveToJson()
+    {
+        var json = JsonUtility.ToJson(_customCommands);
+        File.WriteAllText(GetPath(), json);
+    }
+
+    public Dictionary<string, string> LoadFromJson()
+    {
+        Debugs.Log("Attempting to load CustomCommands: " + Application.persistentDataPath);
+        var path = GetPath();
+        if (File.Exists(path))
+        {
+            var json = File.ReadAllText(path);
+            return JsonUtility.FromJson<SerializationWrapper>(json)?.ToDictionary()
+                   ?? new Dictionary<string, string>();
+        }
+
+        Debugs.LogError("Could not find CustomCommands!");
+        return new Dictionary<string, string>();
+    }
+
+    private void EditCommand(ChatCommand obj)
+    {
+        var messageText = obj.MessageText;
+        var newResponseText = string.Join(" ", messageText.Split(" ")[1..]);
+        var commandToChange = messageText.Split(" ")[0].Replace(obj.Identifier, "");
+        Debugs.Log($"CommandToChange {commandToChange}");
+        if (!_customCommands.TryGetValue(commandToChange, out var response))
+        {
+            var invalidCommand = "Could not find command or command is invalid: " + commandToChange;
+            Debugs.LogWarning(invalidCommand);
+            TwitchApi.SendChatMessage(invalidCommand);
+            return;
+        }
+
+        _customCommands[commandToChange] = newResponseText;
+        TwitchApi.SendChatMessage($"{commandToChange} successfully edited!");
+        SaveToJson();
+    }
+
+    private void AddCommand(ChatCommand obj)
+    {
+        var messageText = obj.MessageText;
+        var commandToAdd = messageText.Split(" ")[0].Replace(obj.Identifier, "");
+        Debugs.Log($"CommandToAdd: {commandToAdd}");
+        if (_customCommands.TryGetValue(commandToAdd, out var response))
+        {
+            TwitchApi.SendChatMessage($"{commandToAdd} Already exists! did you mean !cmdedit?");
+            return;
+        }
+
+        var newResponseText = string.Join(" ", messageText.Split(" ")[1..]);
+        _customCommands[commandToAdd] = newResponseText;
+        var success = $"{commandToAdd} Successfully added!";
+        Debugs.Log(success);
+        TwitchApi.SendChatMessage(success);
+        SaveToJson();
     }
 
 
@@ -104,5 +182,20 @@ public class TwitchChatHandler
         var chatterUserName = eventNotification?["chatter_user_name"]?.ToString();
 
         return new ChatMessage(messageText, chatterUserId, chatterUserLogin, chatterUserName);
+    }
+
+    [Serializable]
+    private class SerializationWrapper
+    {
+        public List<string> keys;
+        public List<string> values;
+
+        public Dictionary<string, string> ToDictionary()
+        {
+            var dict = new Dictionary<string, string>();
+            for (var i = 0; i < keys.Count; i++)
+                dict[keys[i]] = values[i];
+            return dict;
+        }
     }
 }
